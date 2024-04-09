d63e0dd98dcfd59a988ac4dadc469a372f90395b
blank-background <89756413+blank-background@users.noreply.github.com>
2024-04-09 00:23:05 -0500

    functional layout

--- CyberCoreEngine/src/config.h
+++ CyberCoreEngine/src/config.h
@@ -1,19 +1,12 @@
 #pragma once
 
 #include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <vector>
 
-#include <SDL.h>
-
-
-#define GLFW_INCLUDE_VULKAN
-
-#include <stdexcept>
-#include <cstdlib>
-
-#include "../CyberCore/Vendor/imgui/imgui.h"
-#include "../CyberCore/Vendor/imgui/backend/imgui_impl_glfw.h"
-#include "../CyberCore/Vendor/imgui/backend/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
--- CyberCoreEngine/src/main.cpp
+++ CyberCoreEngine/src/main.cpp
@@ -1,5 +1,6 @@
 #include "config.h"
 
+
 // global
 #define WIDTH 1920
 #define HEIGHT 1080
